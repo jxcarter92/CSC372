c0b358f72949e050022045803ccbf03fa23f3ff5
jxcarter92 <jose.carter@csuglobal.edu>
2026-04-16 12:24:18 -0600

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -1,13 +1,21 @@
+/*
+ * Author: Jose Carter
+ * Course: CSC372
+ * Date: 
+ * Description: This class represents a BankAccount and contains
+ * methods for deposit, withdrawal, and account summary.
+ */
+
 package superClass;
 
 
 public class BankAccount {
 
-	private String firstName;
+	private String firstName;//private fields
 	private String lastName;
 	private int accountID;
 	private double balance;
-	
+	//getter setters
 	public String getFirstName() {
 		return firstName;
 	}
@@ -31,7 +39,7 @@ public class BankAccount {
 		return balance;
 	}
 	
-	
+	// constructor using fields. Set balance to 0
 	public BankAccount(String firstName, String lastName, int accountID) {
 		super();
 		this.firstName = firstName;
@@ -39,16 +47,17 @@ public class BankAccount {
 		this.accountID = accountID;
 		this.balance = 0; 
 	}
+	//deposit method for initial bank balance(balance = balance + deposit). this is called in main using instance account.deposit(1500);
 	public void deposit(double deposit) {
 		balance  += deposit; 
 	}
-	
+	//balance for withdraw user choose from initial deposit. this is used in the processWithdrawal method in the checkingAccount class
 	public void withdrawal(double withdrawal) {
 		balance -= withdrawal;
 		
 	}
 	
-	public void accountSummary() {
+	public void accountSummary() {//also called in main instance would be CheckingAccount account = new CheckingAccount("Jose", "carter", 1223, 0.05);
 		
 		System.out.println("First name is: " + getFirstName());
 		System.out.println("Last name is: " + getLastName());
